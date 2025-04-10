69280c3e3039ec1d3c522b35c53b68a24fb0fa11
Aileen <aileenmagadanruiz@gmail.com>
2025-04-10 16:29:39 +0000

    if else statement

---
+++ if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    //Exercise 1
+    int num1;
+    int num2;
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
+if (num1==num2){
+    printf("%d is equal to %d\n", num1, num2);
+}else{
+    printf("%d is NOT equal to %d\n", num1, num2);
+}
+//Excersice 2
+
+}
