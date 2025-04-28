868a9b89e8ffc795d43a788f8293f8e161fb7495
Aileen <aileenmagadanruiz@gmail.com>
2025-04-28 16:30:44 +0000

    If challenge

--- if-else.c
+++ if-else.c
@@ -6,11 +6,43 @@ int main(){
     int num2;
     printf("Enter two numbers:");
     scanf("%d %d", &num1, &num2);
-if (num1==num2){
+if (num1 == num2){
     printf("%d is equal to %d\n", num1, num2);
-}else{
+} else {
     printf("%d is NOT equal to %d\n", num1, num2);
 }
 //Excersice 2
-
+int number;
+printf("Enter a number");
+scanf("%d", &number);
+if(number %2==0 ){
+    printf("%d is an even number", number);
+} else {
+    printf("%d is not an even number \n", number);
+}
+//Exercise 3
+int day;
+printf("\n Write a number between 1 and 7");
+scanf("%d",&day);
+if(day ==1){
+    printf("\n it is Sunday ");
+}
+if(day==2){
+    printf("\n it is Monday");
+}
+if(day==3){
+    printf("\n it is Tuesday");
+}
+if(day==4){
+    printf("\n it is Wednesday");
+}
+if (day==5){
+    printf("\n it is Thursday");
+}
+if(day==6){
+    printf("\n it is Friday");
+}
+if(day==7){
+    printf("\n it is Saturday");
+}
 }
