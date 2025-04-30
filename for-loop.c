f54ced3ff2d264d1002040458e66ed73e014c4ae
Aileen <aileenmagadanruiz@gmail.com>
2025-04-30 18:01:02 +0000

    Mul chart

---
+++ for-loop.c
@@ -0,0 +1,25 @@
+//Print numbers from 1 to 10 
+#include <stdio.h>
+
+int main() {
+    
+    //Exercise1
+    int n;
+    printf("Enter a positive number.");
+    scanf("%d",&n);
+    
+    int m;
+    int sum = 0;
+    for(m=1; m <n + 1; ++m){
+        sum = sum + m; 
+    }
+    printf("%d \n", sum);
+
+    //Exercise 2
+    int f;
+    printf("Enter a positive number.");
+    scanf("%d",&f);
+
+    int multiplication = 1;
+    
+}
--- if-challenge.c
+++ if-challenge.c
@@ -5,7 +5,7 @@ int main(){
     int year;
     scanf("%d", &year);
 
-    if(year % 4 == 0 && year % 100 != 0){
+    if((year % 4 == 0 && year % 100 != 0)||year % 400){
         printf("%d is a leap year.\n", year);
     }else{
         printf("%d is NOT a leap year.\n", year);
