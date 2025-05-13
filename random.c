f2b938a1a68e9f154a5e9c2e77fd18662b3e29c7
Aileen <aileenmagadanruiz@gmail.com>
2025-05-13 14:48:16 +0000

    random numbers

---
+++ random.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    
+    srand(time(NULL));
+    int diceRoll = (rand() % 6) + 1; // Generates a number between 1 and 6
+    printf("You rolled a: %d\n", diceRoll);
+    return 0;
+}
